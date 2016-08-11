e2452999de41b082f682bda71ca8ae4597398745
Dan Maltbie <dmaltbie@daloma.org>
2016-08-10 17:06:14 -0700

    added si446x_dbus_interface to si446x module level symbols

--- si446x/si446x/__init__.py
+++ si446x/si446x/__init__.py
@@ -6,6 +6,9 @@ Si446x Packet Driver: Native Python implementation for Si446x Radio Device
 """
 __all__ = ['si446xact', 'si446xdef', 'si446xdvr', 'si446xFSM', 'si446xradio', 'si446xcfg', 'si446xtrace']
 
-__version__ = '0.1.3'
+from si446xdvr import si446x_dbus_interface
+
+
+__version__ = '0.0.4'
 
 print 'si446x driver version {}'.format(__version__)
