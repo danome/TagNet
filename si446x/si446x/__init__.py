86e16626e235441498523b20599d7b9fd65b02f3
Dan Maltbie <dmaltbie@daloma.org>
2016-10-04 00:42:42 -0700

    WIP: SI446x cleanup init

--- si446x/si446x/__init__.py
+++ si446x/si446x/__init__.py
@@ -4,16 +4,17 @@ Si446x Packet Driver: Native Python implementation for Si446x Radio Device
 
 @author: Dan Maltbie
 """
-#__all__ = ['si446xact', 'si446xdef', 'si446xdvr', 'si446xFSM', 'si446xradio', 'si446xcfg', 'si446xtrace']
+# ['si446xact', 'si446xdef', 'si446xdvr', 'si446xFSM', 'si446xradio', 'si446xcfg', 'si446xtrace']
 
-from .si446xFSM    import Events, Actions, States, table
+from .si446xact    import *
 from .si446xdef    import *
-from .si446xcfg    import *
 from .si446xdvr    import *
+from .si446xFSM    import Events, Actions, States, table
+from .si446xradio  import *
+from .si446xcfg    import *
+from .si446xtrace  import *
 
 __all__ = (si446xdvr.__all__ + si446xact.__all__ + si446xradio.__all__ + si446xtrace.__all__)
-
-#from si446xdvr import si446x_dbus_interface, reactor_loop
 
 __version__ = '0.0.8'
 
