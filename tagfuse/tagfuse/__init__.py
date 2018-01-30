d808580d34aaf27d261b08fca48236b88766a1e7
Daniel J Maltbie <dmaltbie@daloma.org>
2018-01-30 01:27:33 -0800

    TagNet: move version print to __main__.py
    - removed from __init__.py

--- tagfuse/tagfuse/__init__.py
+++ tagfuse/tagfuse/__init__.py
@@ -3,16 +3,15 @@ Tagnet: TagNet Fuse Filesystem for accessing Tag Storage
 
 @author: Dan Maltbie, (c) 2017
 """
-
 import os
 import sys
 
 # If we are running from the source directory, try
 # to load the module from there first.
 basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print('{} init: argv:{}, basedir:{}'.format(os.path.basename(basedir),
-                                            sys.argv[0],
-                                            basedir,))
+# zzz print('{} init: argv:{}, basedir:{}'.format(os.path.basename(basedir),
+# zzz                                           sys.argv[0],
+# zzz                                           basedir,))
 if (os.path.exists(basedir)
     and os.path.exists(os.path.join(basedir, 'setup.py'))):
     add_dirs = [os.path.join(basedir, os.path.basename(basedir)),
@@ -28,6 +27,3 @@ from radioimage  import *
 from radiofile   import *
 from taghandlers import *
 from TagFuseTree import TagFuseFileTree
-
-__version__ = '0.0.9'
-print('tagfuse version: ', __version__)
