ad4011eaaacd2bba59fdf2ef978d9c55c0d9895b
Daniel J Maltbie <dmaltbie@daloma.org>
2018-01-31 22:41:53 -0800

    TagNet: added fuse access to dblk record and sync offset
    Can now find out details about the dblk file system by looking at
    file entries in the Tag Fuse sub-directory 'tag/sd/0/dblk'
    
    - DblkLastRecNum (tagname: '.recnum')
      the last record number written to the dblk file
    
    - DblkLastRecOffset (tagname: '.last_rec')
      the byte offset of the last record written
    
    - DblkLastSyncOffset  (tagname: '.last_sync')
      the byte offset of the last SYNC record written
    
    Each time the file is accessed, a network request is sent to the
    tag to retrieve its current value. If the tag is not in network
    range, the last value read is returned.
    
    The value returned from the tag can be read either of two ways:
    (1) looking at the file size through a stat operation (e.g., 'ls -al')
    (2) reading the contents of the file

--- tagfuse/tagfuse/TagFuseTree.py
+++ tagfuse/tagfuse/TagFuseTree.py
@@ -46,8 +46,10 @@ def TagFuseFileTree(radio):
                                 ('',     FileHandler(S_IFDIR, 0o751, 3)),
                                 ('0',    ByteIOFileHandler(radio, S_IFREG, 0o220, 1)),
                             ]))),
-                            ('note',    DblkIONoteHandler(radio, S_IFREG, 0o220, 1)
-                            )
+                            ('note',    DblkIONoteHandler(radio, S_IFREG, 0o220, 1)),
+                            ('.recnum',   ByteIOFileHandler(radio, S_IFREG, 0o444, 1)),
+                            ('.last_rec', ByteIOFileHandler(radio, S_IFREG, 0o444, 1)),
+                            ('.last_sync',ByteIOFileHandler(radio, S_IFREG, 0o444, 1)),
                         ]))),
                         ('panic', DirHandler(OrderedDict([
                             ('',    FileHandler(S_IFDIR, 0o751, 4)),
