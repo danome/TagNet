ad566617d908c8cfb9781fbd52e80f452e02c2e1
Daniel J Maltbie <dmaltbie@daloma.org>
2018-02-03 02:39:42 -0800

    TagNet: removed dblk numbering in tagfuse
    - no longer need to specify a number after dblk/byte
    - dblk/byte is now a file that can be read to retrieve dblk
    contents

--- tagfuse/tagfuse/TagFuseTree.py
+++ tagfuse/tagfuse/TagFuseTree.py
@@ -41,11 +41,8 @@ def TagFuseFileTree(radio):
                             ('',    FileHandler(S_IFDIR, 0o751, 4)),
                         ]))),
                         ('dblk',  DirHandler(OrderedDict([
-                            ('',     FileHandler(S_IFDIR, 0o751, 4)),
-                            ('byte',   DirHandler(OrderedDict([
-                                ('',     FileHandler(S_IFDIR, 0o751, 3)),
-                                ('0',    ByteIOFileHandler(radio, S_IFREG, 0o220, 1)),
-                            ]))),
+                            ('',     FileHandler(S_IFDIR, 0o751, 7)),
+                            ('byte',   ByteIOFileHandler(radio, S_IFREG, 0o444, 1)),
                             ('note',    DblkIONoteHandler(radio, S_IFREG, 0o220, 1)),
                             ('.recnum',   ByteIOFileHandler(radio, S_IFREG, 0o444, 1)),
                             ('.last_rec', ByteIOFileHandler(radio, S_IFREG, 0o444, 1)),
