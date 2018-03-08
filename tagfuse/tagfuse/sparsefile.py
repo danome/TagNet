c00e20ec7c874069ffaf9a13db92a35c374a8d30
Daniel J Maltbie <dmaltbie@daloma.org>
2018-03-07 23:26:33 -0800

    TagFuse: major improvement to sparsefile coalescing blocks together

--- tagfuse/tagfuse/sparsefile.py
+++ tagfuse/tagfuse/sparsefile.py
@@ -14,67 +14,125 @@ class SparseFile(Chest):
     Sparse file provides a large file that takes up only the
     space used by actual data written.
     '''
-    def __init__(self, filename):
-        print('sparsefile init', filename)
-        Chest.__init__(self, path=filename)
-        self.filename = filename
+    def __init__(self, dirname):
+        print('*** sparsefile init', dirname)
+        super(SparseFile, self).__init__(path=dirname)
+#        Chest.__init__(self, path=dirname)
+        self.dirname = dirname
 
-    def coalesce(self, offset):
+    def _check_bytes(self):
+        if len(self):
+            for a_s in sorted(self.keys()):
+                block = self[a_s]
+                a_e = a_s + len(block)
+                set_a = Set(range(a_s, a_e))
+                for b_s in sorted(self.keys()):
+                    if a_s == b_s:
+                        continue
+                    block = self[b_s]
+                    b_e = b_s + len(block)
+                    set_b = Set(range(b_s, b_e))
+                    common = set_a.intersection(set_b)
+                    if len(common):
+                        # zzz
+                        print("*** _check_bytes error", common)
+        else:
+            print("*** _check_bytes empty list")
+
+    def _coalesce(self):
         '''
         coalesce smaller blocks into larger blocks
 
-        If two blocks are adjacent, then combine into one.
+        Combine blocks that overlap in the sparse dictionary and
+        repeat until all overlaps have been combined.
+
+        algorithm:
+        1 first sort the list of keys in the sparse dictionary,
+          we want to process in ascending order to reduce the
+          number of test conditions.
+        2 next, start with the first two block in the list.
+        3 compare the two blocks (call them first and second)
+          i)   if first block ends before the second block begins,
+               first block is no longer of interest, replace it
+               with the second block
+          ii)  else if first block is strict superset of the second,
+               discard second block
+          iii) else part of first block overlaps with the beginning
+               of the second block, combine the two blocks into one
+               which becomes the new first block. discard the
+               the second block
+        4 get new second block from the list and repeat steps 3-4
+          until no more blocks in the list
         '''
+        # list is sorted, so b_s (second block) can never be less
+        # than a_s (first block) in the comparisons
         block_list = sorted(self.keys())
-        while (block_list):
-            # zzz print('coalesce', block_list)
-            this_pos = block_list[0]
-            if this_pos < offset:
-                del self[this_pos]
+        if block_list is None:
+            return
+        a_s   = block_list.pop(0)
+        a_blk = self[a_s]
+        a_e   = a_s + len(a_blk)
+        while (len(block_list)):
+            # zzz print('*** coalesce block_list', block_list)
+            # first time only, start and end addresses for a_blk
+            # start and end address for b_blk
+            b_s   = block_list.pop(0)
+            b_blk = self[b_s]
+            b_e   = b_s + len(b_blk)
+            # zzz print('*** _coalesce loop: a_s: {}, a_e: {}, b_s: {}, b_e: {}'.format(
+            # zzz    a_s, a_e, b_s, b_e))
+            # case (3.i) first ends before second begins
+            if (a_e < b_s):
+                # zzz print('*** coalesce skip to next')
+                # no more overlap, move on starting with b_blk
+                a_s   = b_s
+                a_blk = b_blk
+                a_e   = b_e
                 continue
-            if (len(block_list) == 1):
-                break
-            next_pos = block_list[1]
-            # zzz print('coalesce', this_pos, next_pos)
-            if (next_pos == this_pos + len(self[this_pos])):
-                self[this_pos] = self[this_pos] + self[next_pos]
-                del self[next_pos]
-                del block_list[1]
+            # case (3.ii) first subsumes second
+            elif (a_e >= b_e):
+                # zzz print("coalesce replace", a_s, a_e, b_s, b_e)
+                del self[b_s]
+                continue
+            # case (3.iii) first overlaps with beginning of second
+            elif (a_e < b_e):
+                # zzz print("coalesce combine",
+                # zzz       len(a_blk), hexlify(a_blk), len(b_blk), hexlify(b_blk))
+                # slice the first part of a_blk up to the overlap
+                # with b_blk and then combine with b_blk
+                new_block = a_blk[:b_s-a_s]
+                new_block.extend(b_blk)
+                # zzz print("coalesce combine new", len(new_block), hexlify(new_block))
+                # replace old a_blk with new_block and delete b_blk.
+                self[a_s] = a_blk = new_block
+                a_e = b_e
+                del self[b_s]
+                continue
+            # not expected
             else:
-                del block_list[0]
+        self._check_bytes()
 
     def add_bytes(self, offset, buf):
         """
         Adds byte data to the sparsefile.
 
         Skip data that has already been added and fill in
-        the holes by inserting new bytes.
+        the holes by inserting any new bytes.
 
-        Returns total bytes processed.
+        Returns total bytes processed (added + existing).
         """
-        # zzz print('add',offset, len(buf), buf)
-        if len(self):
-            a_s = offset
-            a_e = a_s + len(buf)
-            b_e = 0
-            for b_s, block in self.overlay(a_s, a_e):
-                b_e = b_s + len(block)
-                # zzz print('add bytes', a_s, a_e, b_s, b_e, block)
-                if (a_s < b_s):
-                    first = a_s - offset
-                    last = b_s - offset
-                    self[a_s] = buf[first:last]
-                    a_s = b_e
-            if (b_e == 0):
+        if len(buf) == 0:
+            return 0
+
+        self._check_bytes()
+        # zzz print('*** add_bytes, offset/len', offset, len(buf))
+        try: # replace if offset exists and buffer is longer
+            if len(self[offset]) < len(buf):
                 self[offset] = buf
-            elif (a_e > b_e):
-                first = b_e - offset
-                last = a_e - offset
-                self[b_e] = buf[first:last]
-            self.coalesce(0)
-        else:
-            self[offset] = buf
-        print('add bytes', len(buf), offset)
+        except KeyError:
+            self[offset] = buf  # otherwise add new entry
+        self._coalesce()
+        self._check_bytes()
         return len(buf)
 
     def get_bytes_and_holes(self, offset, size):
@@ -99,51 +157,56 @@ class SparseFile(Chest):
         as the associated hole or data block is added to the
         rtn value.
         """
+        self._check_bytes()
         rtn = []
         if len(self):
             a_s = offset
             a_e = b_e = offset + size
-            for b_s, block in self.overlay(offset, a_e):
+            for b_s, block in self._overlay(a_s, a_e):
                 b_e = b_s + len(block)
-                # zzz
-                print('get', a_s, a_e, b_s, b_e, hexlify(block[:24]))
+                # zzz print('*** get_bytes/holes', a_s, a_e, b_s, b_e, hexlify(block[:24]))
                 if (a_s < b_s):        # add hole if needed
                     rtn.append([a_s, b_s])
                     a_s  = b_s
                 first = a_s - b_s      # add overlap data
                 last  = min(a_e, b_e) - b_s
-                rtn.append(block[first:last])
-                a_s += last
-                if (a_s > a_e):      # advanced to end of req
-                    break            # done
-            if (a_e > b_e):          # final hole if needed
+                chunk = block[first:last]
+                rtn.append(chunk)
+                a_s += len(chunk)
+                #a_s += last
+                if (a_s > a_e):        # done
+                    break
+            if (a_e > b_e):            # final hole if needed
                 rtn.append([b_e, a_e])
+        self._check_bytes()
+        # zzz print(rtn)
         return rtn
 
 
-    def overlay(self, start, size):
+    def _overlay(self, a_s, a_e):
         """
-        Finds the set of data blocks that are within the
-        range of offset and size. From this, the list of
-        blocks (bytes) and gaps (holes) can be determined.
+        Finds the set of data blocks that are within the range
+        of starting (a_s) and ending (a_e) byte positions.
+        This information is used by the caller determine the
+        list of blocks (bytes) and gaps (holes).
 
-        Returns list of indices of the blocks within the
-        range.
+        Returns list of (offset, block) tuples of the blocks
+        within the range.
         """
+        self._check_bytes()
         block_list = []
         if len(self):
-            a_s = start
-            a_e = start + size
             for b_s in sorted(self.keys()):
                 block = self[b_s]
                 b_e = b_s + len(block)
-                # zzz print('overlay', a_s, a_e, b_s, b_e, block)
-                if (a_s > b_e): # offset after this block
+                # zzz print('*** _overlay', a_s, a_e, b_s, b_e, len(block))
+                if (a_s >= b_e): # offset after this block
                     continue    # skip
-                if (a_e < b_s): # offset+size before this block
+                if (b_s > a_e): # offset+size before this block
                     break       # done
-                block_list.append([b_s, block]) # block overlaps
-        # zzz print('block list', block_list)
+                block_list.append((b_s, block)) # block overlaps
+        # zzz print('*** overlay block list', len(block_list))
+        self._check_bytes()
         return block_list
 
 if __name__ == '__main__':
