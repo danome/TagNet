3f3bd998ab742b5b40a31bdb31a57de9eb7029a4
Daniel J Maltbie <dmaltbie@daloma.org>
2018-04-24 00:35:51 -0700

    Jupyter: improve background startup of jupyter server

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -2,4 +2,5 @@
 #cd o/Open/TagNet/
 sudo jupyter nbextension enable --py --sys-prefix widgetsnbextension
 jupyter nbextension enable --py gmaps
-nohup jupyter notebook --no-browser --port=8999 &
+#nohup jupyter notebook --no-browser --port=8999 &
+su pi -c "nohup jupyter notebook --browser=false --allow-root --port=9000 --notebook-dir /home/pi/Desktop/TagNet&"
