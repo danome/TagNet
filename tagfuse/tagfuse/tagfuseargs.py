3ef48e84fa44cbfe328c5680d6409044898ee977
Daniel J Maltbie <dmaltbie@daloma.org>
2018-05-01 01:40:29 -0700

    TagFuse: bump version to 0.1.0

--- tagfuse/tagfuse/tagfuseargs.py
+++ tagfuse/tagfuse/tagfuseargs.py
@@ -2,7 +2,7 @@ import os
 import sys
 import argparse
 
-__version__ = '0.0.34'
+__version__ = '0.1.0'
 
 # If we are running from the source package directory, try
 # to load the module from there first.
