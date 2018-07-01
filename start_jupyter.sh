a08934eb9d74a84f92a647fe0d2ed18ff364ffcb
Daniel J Maltbie <dmaltbie@daloma.org>
2018-07-01 16:55:51 -0700

    TagNet: increase process priority in start_jupyter.sh script
    - also set the executable file mode

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -3,4 +3,4 @@
 sudo jupyter nbextension enable --py --sys-prefix widgetsnbextension
 jupyter nbextension enable --py gmaps
 #nohup jupyter notebook --no-browser --port=8999 &
-su pi -c "nohup jupyter notebook --browser=false --allow-root --port=9000 --notebook-dir /home/pi/Desktop/TagNet&"
+su pi -c "nohup nice --adjustment=-20 jupyter notebook --browser=false --allow-root --port=9000 --notebook-dir /home/pi/Desktop/TagNet&"
