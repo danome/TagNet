c902f3a7e62215119264c26b496801aa5aecaadf
Daniel J Maltbie <dmaltbie@daloma.org>
2018-07-31 22:47:09 -0700

    TagNet: revised from 0.1.4 to 0.2.0, Protocol Version 2

--- tagnet/tagnet/__init__.py
+++ tagnet/tagnet/__init__.py
@@ -14,7 +14,7 @@ if (os.path.exists(os.path.join(basedir, 'setup.py')) and
     sys.path.insert(0, os.path.join(basedir, 'tagnet'))
     print '\n'.join(sys.path)
 
-__version__ = '0.1.4'
+__version__ = '0.2.0'
 print 'TagNet Driver Version {}'.format(__version__)
 
 from .tagnames import *
